0695a9f34e78e5248ce442c219748e338fc967e2
franco luca parera <francolucap1@gmail.com>
2025-03-06 22:15:01 -0300

    intento 2

---
+++ .history/next.config_20250305203434.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    experimental: {
+      appDir: true, // Habilita el directorio "app" en Next.js
+    },
+  };
+  
+  module.exports = nextConfig;
+  
