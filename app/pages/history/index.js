b104e91300e37bb047f3cf74ebf2bf1ceb5eabfb
FranComputer <105250105+francoluca35@users.noreply.github.com>
2025-03-08 15:10:21 -0300

    Update index.js

--- app/pages/history/index.js
+++ app/pages/history/index.js
@@ -24,18 +24,6 @@ export default function Nosotros() {
             Han pasado más de 25 años. Hoy sentimos orgullo de haber ubicado a
             Pauna entre las marcas líderes del mercado.
           </p>
-          <p className="text-gray-600 mb-4">
-            Nos enfocamos en lograr que la tarea de los profesionales de la
-            panadería, pastelería, gastronomía, hotelería y food service sea lo
-            más cómoda y eficiente posible. Trabajamos juntos en la creación de
-            soluciones que los ayuden a elaborar los mejores productos para sus
-            clientes.
-          </p>
-          {/* Imagen en mobile entre el texto y el botón */}
-          <div className="w-full flex justify-center md:hidden mb-4">
-             
-          </div>
-         
         </div>
       </section>
     </div>
