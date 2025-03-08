71daf6b2d29685c8b43bb29eff56778294b625b3
FranComputer <105250105+francoluca35@users.noreply.github.com>
2025-03-08 14:34:56 -0300

    cambios en las imagenes

--- app/pages/history/index.js
+++ app/pages/history/index.js
@@ -34,7 +34,7 @@ export default function Nosotros() {
           {/* Imagen en mobile entre el texto y el botón */}
           <div className="w-full flex justify-center md:hidden mb-4">
             <Image
-              src="/Assets/person1.jpg"
+              src="/Assets/1.png"
               width={500}
               height={500}
               alt="Nosotros"
