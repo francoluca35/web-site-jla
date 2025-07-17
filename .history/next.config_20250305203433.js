9cf7bd9e6c1e2846044f9389338d379d0c715b1f
franco luca parera <francolucap1@gmail.com>
2025-07-17 20:18:42 -0300

    cambios en pagina ceo

--- .history/next.config_20250305203433.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    experimental: {
-      appDir: true, // Habilita el directorio "app" en Next.js
-    },
-  };
-  
-  module.exports = nextConfig;
-  
--- app/page.jsx
+++ app/page.jsx
@@ -9,20 +9,45 @@ import Stecnico from "./pages/Stecnico";
 import Clientes from "./pages/Clientes";
 import Contacto from "./pages/Contacto";
 import Footer from "./pages/Footer";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div>
-      <Navbar />
-      <Inicio />
-      <Nosotros />
-      <Producto />
-      <Clientes />
-      <Stecnico />
-      <Contacto />
-      <ArrowUp />
-      <FloatBars />
-      <Footer />
-    </div>
+    <>
+      <Head>
+        <title>JLA Técnicos - Equipamiento Gastronómico Industrial</title>
+        <meta
+          name="description"
+          content="Hornos industriales, panificadoras, freidoras, amasadoras y mucho más. Soluciones gastronómicas profesionales para cocinas industriales y panaderías."
+        />
+        <meta
+          name="keywords"
+          content="hornos industriales, panificadoras, freidoras industriales, amasadoras, gastronomía industrial, equipamiento gastronómico"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta property="og:title" content="JLA Técnicos" />
+        <meta
+          property="og:description"
+          content="Equipamiento gastronómico profesional e industrial. Desde 2000 trabajando con vos."
+        />
+        <meta
+          property="og:image"
+          content="https://jlatecnicos.com/Assets/imagen-seo.jpg"
+        />
+        <meta property="og:url" content="https://jlatecnicos.com" />
+      </Head>
+      <div>
+        <Navbar />
+        <Inicio />
+        <Nosotros />
+        <Producto />
+        <Clientes />
+        <Stecnico />
+        <Contacto />
+        <ArrowUp />
+        <FloatBars />
+        <Footer />
+      </div>
+    </>
   );
 }
