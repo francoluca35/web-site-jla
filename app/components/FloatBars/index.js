3008343c32d43361eace78e928004a9b7e017798
franco luca parera <francolucap1@gmail.com>
2025-07-17 20:42:13 -0300

    prueba

--- app/components/FloatBars/index.js
+++ app/components/FloatBars/index.js
@@ -1,8 +1,8 @@
-'use client';
-import { useState } from 'react';
-import { useRouter } from 'next/navigation';
-import { FaWhatsapp } from 'react-icons/fa';
-import Image from 'next/image';
+"use client";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { FaWhatsapp } from "react-icons/fa";
+import Image from "next/image";
 
 export default function FloatBars() {
   const [hovered, setHovered] = useState(null);
@@ -10,66 +10,96 @@ export default function FloatBars() {
   const router = useRouter();
 
   const handleNavigation = () => {
-  router.push( `/producatalogo ` ); 
-};
+    router.push(`/producatalogo`);
+  };
 
   return (
     <>
       <div className="fixed right-0 top-1/2 transform -translate-y-1/2 flex flex-col space-y-2 z-50">
         {/* Botón de Catálogo */}
-        <a 
+        <a
           onClick={handleNavigation}
-          target="_blank" 
-          rel="noopener noreferrer" 
+          target="_blank"
+          rel="noopener noreferrer"
           className="bg-gray-800 text-white w-16 h-16 flex flex-col items-center justify-center rounded-l-lg shadow-lg hover:bg-gray-700 transition-all relative"
-          onMouseEnter={() => setHovered('catalogo')}
+          onMouseEnter={() => setHovered("catalogo")}
           onMouseLeave={() => setHovered(null)}
         >
-          <Image 
+          <Image
             src="/Assets/ic_catalogo.png"
             width={25}
-            height={30} 
+            height={30}
             alt="Catálogo"
-          /> 
-          {hovered === 'catalogo' && (
-            <span className="bottom-[-20px] text-xs text-white px-2 py-1 rounded-md">Catálogo</span>
+          />
+          {hovered === "catalogo" && (
+            <span className="bottom-[-20px] text-xs text-white px-2 py-1 rounded-md">
+              Catálogo
+            </span>
           )}
         </a>
-        
+
         {/* Botón de WhatsApp */}
         <button
           onClick={() => setIsModalOpen(true)}
           className="bg-green-600 text-white w-16 h-16 flex flex-col items-center justify-center rounded-l-lg shadow-lg hover:bg-green-800 transition-all relative"
-          onMouseEnter={() => setHovered('whatsapp')}
+          onMouseEnter={() => setHovered("whatsapp")}
           onMouseLeave={() => setHovered(null)}
         >
           <FaWhatsapp className="text-4xl" />
-          {hovered === 'whatsapp' && (
-            <span className="bottom-[-20px] text-xs text-white px-2 py-1 rounded-md">WhatsApp</span>
+          {hovered === "whatsapp" && (
+            <span className="bottom-[-20px] text-xs text-white px-2 py-1 rounded-md">
+              WhatsApp
+            </span>
           )}
         </button>
       </div>
-      
+
       {/* Modal de WhatsApp */}
       {isModalOpen && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
           <div className="bg-white p-6 rounded-lg shadow-lg text-center">
-            <h2 className="text-lg text-black font-semibold mb-4">Selecciona una opción</h2>
+            <h2 className="text-lg text-black font-semibold mb-4">
+              Selecciona una opción
+            </h2>
             <button
               className="bg-green-500 text-white px-4 py-2 mb-2 rounded-lg w-full hover:bg-green-600"
-              onClick={() => window.open('https://wa.me/5491131536316?text=' + encodeURIComponent('Hola, necesito presupuesto para una reparación'), '_blank')}
+              onClick={() =>
+                window.open(
+                  "https://wa.me/5491131536316?text=" +
+                    encodeURIComponent(
+                      "Hola, necesito presupuesto para una reparación"
+                    ),
+                  "_blank"
+                )
+              }
             >
               🛠 Servicio Técnico
             </button>
             <button
               className="bg-blue-500 text-white px-4 py-2 mb-2 rounded-lg w-full hover:bg-blue-600"
-              onClick={() => window.open('https://wa.me/5491131536316?text=' + encodeURIComponent('Hola, necesito un presupuesto para un horno'), '_blank')}
+              onClick={() =>
+                window.open(
+                  "https://wa.me/5491131536316?text=" +
+                    encodeURIComponent(
+                      "Hola, necesito un presupuesto para un horno"
+                    ),
+                  "_blank"
+                )
+              }
             >
               💰 Presupuesto
             </button>
             <button
               className="bg-gray-800 text-white px-4 py-2 rounded-lg w-full hover:bg-gray-700"
-              onClick={() => window.open('https://wa.me/5491131536316?text=' + encodeURIComponent('Hola, me gustaría un horno como el del catálogo'), '_blank')}
+              onClick={() =>
+                window.open(
+                  "https://wa.me/5491131536316?text=" +
+                    encodeURIComponent(
+                      "Hola, me gustaría un horno como el del catálogo"
+                    ),
+                  "_blank"
+                )
+              }
             >
               📖 Pregunta de Catálogo
             </button>
